bd5aae12aed288d7f09998998ec9165a9cafc937
Guillaume LEDUC <gduc.37@gmail.com>
2023-04-21 10:57:35 +0200

    commit photographer

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,5 +1,5 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, tagline, price, city, country, id } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -7,10 +7,16 @@ function photographerFactory(data) {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+        const link = document.createElement('a');
+        link.href = `./photographer.html?id=${id}`
+        link.appendChild(img);
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
+        const desc = document.createElement('div');
+        desc.innerHTML = `<h3 class='photographer__country'>${city}, ${country}</h3><p class='photographer__tagline'>${tagline}</p><p class='photographer__price'>${price}€/jour</p>`;
+        article.appendChild(link);
         article.appendChild(h2);
+        article.appendChild(desc);
         return (article);
     }
     return { name, picture, getUserCardDOM }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,21 @@
 //Mettre le code JavaScript lié à la page photographer.html
+// Récupération du paramètre id dans l'URL
+const params = new URLSearchParams(window.location.search);
+const idProfil = params.get('id');
+
+// Utilisation de l'id du profil pour récupérer les données associées
+async function photographerData() {
+    const photographer = await fetch('/data/photographers.json').then(r => r.json()).then(r => r.photographers)
+    let photographerProfil ;
+    for (let i=0; i< photographer.length ; i++) {
+        if (photographer[i].id == idProfil) {
+            photographerProfil = photographer[i]
+        }
+    }
+    return photographerProfil
+}
+let profil = null;
+photographerData().then(photographer => {
+    profil = photographer
+    console.log(profil)
+})
