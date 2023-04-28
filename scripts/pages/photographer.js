c312b73f9236cd64dfb989d8a7dff139c7afc1fa
Guillaume LEDUC <gduc.37@gmail.com>
2023-04-28 10:49:33 +0200

    Ajout traitement des photos

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -14,8 +14,36 @@ async function photographerData() {
     }
     return photographerProfil
 }
+function displayData(photographer) {
+    const header = document.querySelector('.photograph-header');
+    const infos = document.createElement('div');
+    infos.classList.add('infos');
+    infos.innerHTML = `<h1 class="photographer-header__name">${photographer.name}</h1><h3 class="photographer-header__location">${photographer.city}, ${photographer.country}</h3><p class="photographer-header__tagline">${photographer.tagline}</p>`
+    const photo = document.createElement('img')
+    photo.setAttribute('src', `assets/photographers/${photographer.portrait}`);
+    photo.classList.add('photographer-header__img');
+    const button = document.querySelector(".contact_button")
+    header.insertBefore(infos, button);
+    header.appendChild(photo);
+}
+
+async function displayGalery(name) {
+    const urlName = name.split(" ").join("");
+    const urlNames = urlName.split("-").join("");
+    const url = `assets/galery/${urlNames}/`;
+    const response = await fetch(url);
+    const body = await response.text();
+    const parser = new DOMParser();
+    const html = parser.parseFromString(body, "text/html");
+    const images = Array.from(html.querySelectorAll(".icon-jpg")).map(
+        (a) => a.getAttribute("href")
+    );
+    console.log(images)
+    return images;
+    
+}
 let profil = null;
 photographerData().then(photographer => {
-    profil = photographer
-    console.log(profil)
+    displayData(photographer);
+    displayGalery(photographer.name);
 })
