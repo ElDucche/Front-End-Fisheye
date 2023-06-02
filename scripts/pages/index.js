ae476ddb3f705c55460ea20f239976c858532a4e
Guillaume LEDUC <gduc.37@gmail.com>
2023-06-02 15:30:26 +0200

    update lien

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,7 +1,7 @@
     async function getPhotographers() {
         // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = await fetch('../../data/photographers.json').then(r => r.json()).then(r => r.photographers);
+        let photographers = await fetch('https://elducche.github.io/Front-End-Fisheye/data/photographers.json').then(r => r.json()).then(r => r.photographers);
         console.log(photographers)
         // et bien retourner le tableau photographers seulement une fois récupéré
         return ({
