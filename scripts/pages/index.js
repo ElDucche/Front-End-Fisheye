851a468515ada61e89fbd99e1f2e553614efb387
Guillaume LEDUC <gduc.37@gmail.com>
2023-06-09 14:07:08 +0200

    correction eslint

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,28 +1,28 @@
-    async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = await fetch('https://elducche.github.io/Front-End-Fisheye/data/photographers.json').then(r => r.json()).then(r => r.photographers);
-        console.log(photographers)
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers]})
-    }
+// const photographerFactory = require('../factories/photographer');
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+async function getPhotographers() {
+  const photographers = await fetch('data/photographers.json')
+    .then((r) => r.json())
+    .then((r) => r.photographers);
+  return {
+    photographers: [...photographers],
+  };
+}
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    };
+async function displayData(photographers) {
+  const photographersSection = document.querySelector('.photographer_section');
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    };
-    
-    init();
-    
+  photographers.forEach((photographer) => {
+    const photographerModel = photographerFactory(photographer);
+    const userCardDOM = photographerModel.getUserCardDOM();
+    photographersSection.appendChild(userCardDOM);
+  });
+}
+
+async function init() {
+  // Récupère les datas des photographes
+  const { photographers } = await getPhotographers();
+  displayData(photographers);
+}
+
+init();
